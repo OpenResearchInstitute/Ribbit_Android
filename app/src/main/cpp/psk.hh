21302fb78fbe55208ae00f329fba439555d3d6f7
Ahmet Inan <xdsopl@gmail.com>
2023-05-12 09:27:10 +0200

    keep limits symmetric so it doesn't interfere with scrambling

--- app/src/main/cpp/psk.hh
+++ app/src/main/cpp/psk.hh
@@ -24,7 +24,7 @@ struct PhaseShiftKeying<2, TYPE, CODE> {
 		if (std::is_integral<code_type>::value)
 			value = std::nearbyint(value);
 		if (std::is_same<code_type, int8_t>::value)
-			value = std::min<value_type>(std::max<value_type>(value, -128), 127);
+			value = std::min<value_type>(std::max<value_type>(value, -127), 127);
 		return value;
 	}
 
@@ -58,7 +58,7 @@ struct PhaseShiftKeying<4, TYPE, CODE> {
 		if (std::is_integral<code_type>::value)
 			value = std::nearbyint(value);
 		if (std::is_same<code_type, int8_t>::value)
-			value = std::min<value_type>(std::max<value_type>(value, -128), 127);
+			value = std::min<value_type>(std::max<value_type>(value, -127), 127);
 		return value;
 	}
 
@@ -99,7 +99,7 @@ struct PhaseShiftKeying<8, TYPE, CODE> {
 		if (std::is_integral<code_type>::value)
 			value = std::nearbyint(value);
 		if (std::is_same<code_type, int8_t>::value)
-			value = std::min<value_type>(std::max<value_type>(value, -128), 127);
+			value = std::min<value_type>(std::max<value_type>(value, -127), 127);
 		return value;
 	}
 
